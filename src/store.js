ca555c33aa889081d53cc1491d4d196664d53ac9
Wes Reid <bwreid@gmail.com>
2018-02-13 14:35:51 -0800

    Gracefully fail if Redux tools not installed

--- src/store.js
+++ src/store.js
@@ -2,12 +2,13 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import rootReducer from  './reducers';
 import logger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+const extension = '__REDUX_DEVTOOLS_EXTENSION__';
 
 export default(initialState) => {
     return createStore(
       rootReducer,
       compose(
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        window[extension] ? window[extension]() : state => state,
         applyMiddleware(logger, thunkMiddleware)
       )
     );
