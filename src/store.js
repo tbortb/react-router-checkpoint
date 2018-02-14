ae17afd6ec1db3f9710da59bb42ca84cccc23022
John <jaboyak@gmail.com>
2018-02-14 12:41:21 -0700

    removed redux dev tools

--- src/store.js
+++ src/store.js
@@ -2,13 +2,11 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import rootReducer from  './reducers';
 import logger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-const extension = '__REDUX_DEVTOOLS_EXTENSION__';
 
 export default(initialState) => {
     return createStore(
       rootReducer,
       compose(
-        window[extension] ? window[extension]() : state => state,
         applyMiddleware(logger, thunkMiddleware)
       )
     );
